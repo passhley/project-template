fc843bd20b3037a6ac2c98bb4b24bdb82f9927c7
pashleyy <64950564+passhley@users.noreply.github.com>
2023-11-18 22:16:17 -0500

    feat: added `.luaurc` to `osyrisrblx/t` after installing

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,8 @@ wally install
 rojo sourcemap -o sourcemap.json bundle.project.json
 wally-package-types -s sourcemap.json Packages/
 wally-package-types -s sourcemap.json ServerPackages/
+
+touch Packages/_Index/osyrisrblx_t@3.0.0/t/.luaurc
+echo "{
+    \"languageMode\": \"nocheck\"
+}" > Packages/_Index/osyrisrblx_t@3.0.0/t/.luaurc
