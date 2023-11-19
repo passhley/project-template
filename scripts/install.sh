7917976e822d6494f2dea4746f82fd12056ff39f
pashleyy <64950564+passhley@users.noreply.github.com>
2023-11-18 22:17:30 -0500

    refactor: added notes to install

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,13 @@ aftman install
 wally install
 rojo sourcemap -o sourcemap.json bundle.project.json
 wally-package-types -s sourcemap.json Packages/
+
+# If you end up not having server packages, make sure to
+# delete the line below this.
 wally-package-types -s sourcemap.json ServerPackages/
 
+# If you get rid of 'osyrisrblx/t', make sure to delete
+# the code below this.
 touch Packages/_Index/osyrisrblx_t@3.0.0/t/.luaurc
 echo "{
     \"languageMode\": \"nocheck\"
